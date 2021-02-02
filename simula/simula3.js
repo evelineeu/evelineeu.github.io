cad43fb355425f7020e7b0920fa9116f05d6a6d4
evelineeu <evellynjco@gmail.com>
2021-02-01 22:49:33 -0300

    upei aqui

--- simula/simula3.js
+++ simula/simula3.js
@@ -1,55 +1,50 @@
-var botao = document.getElementById('botao');
-var limpar = document.getElementById('limpar');
 
-botao.onclick = function retorna()
+function limitante()
 {
-    var inputth2 = document.getElementById('h2').value;
-    var inputto2 = document.getElementById('o2').value;
-    if(inputth2 == '' || inputth2<=0 || inputto2 == '' || inputto2<=0)
+	var o2 = document.getElementById("o2").value;
+	var h2 = document.getElementById("h2").value;
+	o2 = parseFloat(o2);
+	h2 = parseFloat(h2);
+	var grama_h2 =2; 
+	var grama_O2 =32;
+	var grama_H2O = 18;
+	var conta1 =grama_h2* o2;
+	var conta2 = conta1 / (grama_O2/2);
+	var resultado ="";
+	var temp_excesso = 0;
+	
+	var conta3 =(grama_O2/2)* h2;
+	var conta4 = conta3 / grama_h2;
+
+
+	var contah2o = (grama_H2O * h2) / 2;
+	var contah2og = (grama_H2O * o2) / 16;
+
+
+
+	if(conta2<h2)
 	{
-		alert("Insira um valor válido.");
-    }
-    else
-    {
-        inputth2 = parseFloat(inputth2);
-        inputto2 = parseFloat(inputto2);
-        var x = 16*inputth2;
-        var x1 = x/2;
-        var y = 2*inputto2;
-        var y1 = y/16;
-        var r = inputto2 - x1;
-        var r2 = inputth2 - y1;
-        var massa1 = inputto2 + y1;
-        var massa = inputth2 + x1;
-        x = x.toFixed(2);
-        x1 = x1.toFixed(2);
-        y = y.toFixed(2);
-        y1 = y1.toFixed(2);
-        r = r.toFixed(2);
-        r2 = r2.toFixed(2);
-        massa = massa.toFixed(2);
-        massa1 = massa1.toFixed(2);
-        if(x1<inputto2)
-        {
-            var excesso = "2g H2 - 16g O2 <br>" + inputth2 + "g H2 - Xg O2 <br><br> 2 . X = " + x +"<br> X = " + x1 + "<br><br>Para " + inputth2 + "g H2 precisa-se de " + x1 + "g O2<br> " + inputto2 +" - " + x1 + " = " + r + "<br><br>O2 está " + r + " g em excesso<br> O agente limitante é o H2 <br> A massa de H2O formada é de " + massa + "g";
+		resultado = "2g H2 - 16g O2 <br>" +  "XgH2 - " + o2 + " g O2 <br><br><br> 2g H2 X = " + o2 +" gO2 = Xg O2 X 16g O2  <br><br> Xg H2 = " + conta2 + " <br> Portanto para " + o2 + " gO2 é necessário " + conta2 + " gH2";
+		temp_excesso = h2 - conta2;
+		resultado += "<br> <h3>h2 está em excesso e o O2 é o limitante, <br> e o excesso de h2g é: " + temp_excesso + " gH2<br>  o H2O formado é :" + contah2og +" gH2O</h3>";
+	}
 
-        }
-        else if(x1>inputto2)
-        {
-            var excesso = "2g H2 - 16g O2 <br> Xg O2 -" + inputto2 + "g O2 <br><br> 16 . X = " + y +"<br> X = " + y1 + "<br><br>Para " + inputto2 + "g O2 precisa-se de " + y1 + "g H2<br> " + inputth2 +" - " + y1 + " = " + r2 + "<br><br>H2 está " + r2 + " g em excesso<br> O agente limitante é o O2 <br> A massa de H2O formada é de " + massa1 + "g";
-        }
-        else if(x1 == inputto2)
-        {
-            excesso = "Nenhum elemento está em excesso.";
-        }
-        document.getElementById('resultado').innerHTML = excesso;
 
-    }
-}
+	if(conta2>h2)
+	{
+		resultado = "2g H2 - 16g O2 <br>" +  h2 + " gH2 -  xg O2 <br><br><br> 2g H2 X XgO2 = " + h2 +" gH2 X 16g O2  <br><br> Xg O2 = " + conta4 + " <br> Portanto para " + h2 + " gH2 é necessário " + conta4 + " gO2";
+		temp_excesso = o2 - conta4;
+		resultado += " <br> <h3>O2 está em excesso e o h2 é o limitante e o excesso de O2g é: " + temp_excesso + " gO2 <br> o H2O formado é :" + contah2o +" gH2O</h3>";
+		}
 
-limpar.onclick = function limpa()
-{
-    document.getElementById('h2').value = '';
-    document.getElementById('o2').value = '';
-    document.getElementById('resultado').innerHTML = '';
+
+
+	if(h2-conta2==0)
+	{
+		resultado = "2g H2 - 16g O2 <br>" +  "XgH2 - " + o2 + " g O2 <br><br><br> 2g H2 X = " + o2 +" gO2 = Xg O2 X 16g O2  <br><br> Xg H2 = " + conta2 + " <br> Portanto para " + o2 + " gO2 é necessário " + conta2 + " gH2 <br> o H2O formado é :" + contah2o +" gH2O";
+		resultado += "<br> <h3>Não há nenhum elemento em excesso!!!</h3>";
+	}
+
+	document.getElementById("conta").innerHTML = resultado;
+
 }
