bf3ac43459ef8803b5497c10efbb64844448684b
evelineeu <57270931+evelineeu@users.noreply.github.com>
2021-05-03 20:23:35 -0300

    Add files via upload

---
+++ exer2.js
@@ -0,0 +1,80 @@
+<<<<<<< HEAD
+$(function(){
+
+    $("#botao").click(function(){
+        let texto_array = $("#texto_array").val();
+          texto_array = texto_array.replace(/\s+/g, '');
+        let array = texto_array.split(",");
+        let tam = array.length;
+                      if(array[tam-1] == "")
+        {
+            array.pop();
+        }
+        $("#resultado").html(fibonacci(texto_array));
+    })
+
+                     $("#limpar").click(function(){
+                          $("#resultado").text("");
+                                $("#texto_array").val("");
+    })
+
+
+
+    
+    function fibonacci (x) 
+    {
+        let array = new Array;
+        let y = 0;
+        let z = 1;
+        let w = 0;
+        for (let i=0; i<x; i++) 
+        {
+            array.push(w);
+            w = y + z;
+               y = z;
+            z = w;
+               }
+        return array.join(",");
+    }
+
+=======
+$(function(){
+
+    $("#botao").click(function(){
+        let texto_array = $("#texto_array").val();
+          texto_array = texto_array.replace(/\s+/g, '');
+        let array = texto_array.split(",");
+        let tam = array.length;
+                      if(array[tam-1] == "")
+        {
+            array.pop();
+        }
+        $("#resultado").html(fibonacci(texto_array));
+    })
+
+                     $("#limpar").click(function(){
+                          $("#resultado").text("");
+                                $("#texto_array").val("");
+    })
+
+
+
+    
+    function fibonacci (x) 
+    {
+        let array = new Array;
+        let y = 0;
+        let z = 1;
+        let w = 0;
+        for (let i=0; i<x; i++) 
+        {
+            array.push(w);
+            w = y + z;
+               y = z;
+            z = w;
+               }
+        return array.join(",");
+    }
+
+>>>>>>> 590ed5e42e34f077cb6cf948fe67d302a2f74014
+})
